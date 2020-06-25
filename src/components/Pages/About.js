1f2815ca3fdcca52832a46db08689294978d64f9
Allen-12 <allennjuguna@gmail.com>
2020-06-25 23:35:19 +0300

    Initial commit

---
+++ src/components/Pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About()
+{
+    return (
+        <div>
+            <h1>About</h1>
+            <p>This is the TodoList App version 1.0.0. It is part of a React crash course</p>
+        </div>
+    )
+}
